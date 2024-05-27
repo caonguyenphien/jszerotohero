e0bb33b5e42cf805faa595392e7e9a264a91da09
Cao Nguyen Phien <caonguyenphien@gmail.com>
2024-05-27 20:16:32 +0700

    second commit

---
+++ Oject_Java_Cript/2.Mixing_data_funcition_object/app.js
@@ -0,0 +1,17 @@
+// function dientichtamgiac (a,b){
+//     return a * b /2;
+// }
+// function canhhuyentamgiac(a,b){
+//     return Math.sqrt(a**2 +b**2);
+// } "Ctrl + /"
+
+let dientichtamgiac = {
+    a: 3,
+    b: 4,
+    Tamgiaccuatui: function(){
+        return (this.a * this.b) /2;
+    },
+    canhhuyen:function(){
+        return this.a
+    },
+}
